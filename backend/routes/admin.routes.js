b8e4a1b619f69700cc9fefe21ab94d0219af0c9b
ciiroo <diamanquito@gmail.com>
2026-03-10 14:45:35 -0500

    auth.routes.js commit #1

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -3,158 +3,138 @@
  * agrupa todas las rutas de gestion del admin
  */
 
-const express = require('express');
+
+const express = require ('express');
 const router = express.Router();
 
-//importar los middleware
-const { verificarAuth } = require('../middleware/auth');
+// importar los middlewares
+
+const { verificarAuth } = require ('../middleware/auth');
 const { esAdministrador, esAdminOAuxiliar, soloAdministrador } = require ('../middleware/checkRole');
 
-//importar configuracion de multer para la subida de imagenes
-const { upload } = require('../config/multer');
+// importar configuracion de multer para la subida de imagenes
+
+const { upload } = require ('../config/multer');
 
 //importar controladores
-const categoriaController = require('../controllers/categoria.controller');
-const subcategoriaController = require('../controllers/subcategoria.controller');
-const productoController = require('../controllers/producto.controller');
-const usuarioController = require('../controllers/usuario.controller');
-const pedidoController = require('../controllers/pedido.controller');
 
-//restricciones de acceso a las rutas del admin
+const categoriaController = require ('../controllers/categoria.controller');
+const subcategoriaController = require ('../controllers/subcategoria.controller');
+const productoController = require ('../controllers/producto.controller');
+const usuarioController = require ('../controllers/usuario.controller');
+const pedidoController = require ('../controllers/pedido.controller');
+
+// restricciones
 router.use(verificarAuth, esAdminOAuxiliar);
 
-//##-----------------------CATEGORIA-----------------------##//
-
-
 //Rutas de categorias
-//get /api/admin/categorias
+// get /api/admin/categorias
 router.get('/categorias', categoriaController.getCategorias);
 
-//get /api/admin/categoria:id
+// get /api/admin/categorias
 router.get('/categorias/:id', categoriaController.getCategoriasById);
 
-//get /api/admin/categorias/:id/stats
+// get /api/admin/categorias/:id/stats
 router.get('/categorias:id/stats', categoriaController.getEstadisticasCategoria);
 
-//POST /api/admin/categorias
+// POST /api/admin/categorias
 router.post('/categorias', categoriaController.crearCategoria);
 
-//PUT /api/admin/categorias
-router.put('/categorias/:id', categoriaController.actualizarCategoria);
+// PUT /api/admin/categorias
+router.put('/categorias', categoriaController.actualizarCategoria);
 
-//patch /api/admin/categorias:id/toogle desactivar o activar categoria
-router.patch('/categorias/:id/toogle', categoriaController.getCategorias);
+// patch /api/admin/categorias:id / toggle desactivar o activar categoria
+router.patch('/categorias', categoriaController.toggleCategoria);
 
-//delete /api/admin/categorias
-router.get('/categorias/:id', soloAdministrador, categoriaController.eliminarCategoria);
+// delete /api/admin/categorias
+router.get('/categorias/:id', categoriaController.eliminarCategoria);
 
-//##-----------------------SUBCATEGORIA-----------------------##//
+
 
 //Rutas de subcategorias
-//get /api/admin/subcategorias
+// get /api/admin/subcategorias
 router.get('/subcategorias', subcategoriaController.getSubcategorias);
 
-//get /api/admin/subcategoria:id
+// get /api/admin/subcategorias
 router.get('/subcategorias/:id', subcategoriaController.getSubcategoriaById);
 
-//get /api/admin/subcategorias/:id/stats
-router.get('/subcategorias/:id/stats', subcategoriaController.getEstadisticasSubcategoria);
+// get /api/admin/subcategorias/:id/stats
+router.get('/subcategorias:id/stats', subcategoriaController.getEstadisticasSubcategoria);
 
-//POST /api/admin/subcategorias
+// POST /api/admin/subcategorias
 router.post('/subcategorias', subcategoriaController.crearSubcategoria);
 
-//PUT /api/admin/subcategorias
-router.put('/subcategorias/:id', subcategoriaController.actualizarSubcategoria);
+// PUT /api/admin/subcategorias
+router.put('/subcategorias', subcategoriaController.actualizarSubcategoria);
 
-//patch /api/admin/subcategorias:id/toogle desactivar o activar subcategoria
-router.patch('/subcategorias/:id/toogle', subcategoriaController.getSubcategorias);
+// patch /api/admin/subcategorias:id / toggle desactivar o activar categoria
+router.patch('/subcategorias', subcategoriaController.toggleSubcategoria);
 
-//delete /api/admin/subcategorias
-router.delete('/subcategorias/:id', soloAdministrador, subcategoriaController.eliminarSubcategoria);
+// delete /api/admin/subcategorias
+router.get('/subcategorias/:id', subcategoriaController.eliminarSubcategoria);
 
-//##-----------------------PRODUCTOS-----------------------##//
 
-//Rutas de productos
-//get /api/admin/productos
+//Rutas de producto
+// get /api/admin/productos
 router.get('/productos', productoController.getProductos);
 
-//get /api/admin/producto:id
-router.get('/productos/:id', productoController.getProductoById);
-/**
-//get /api/admin/productos/:id/stats
-router.get('/productos/:id/stats', productoController.getEstadisticasProducto);
-*/
-//POST /api/admin/productos
+// get /api/admin/productos
+router.get('/productos/:id', productoController.getProductosById);
+
+// get /api/admin/productos/:id/stats
+//router.get('/productos:id/stats', productoController.getEstadisticasProducto);
+
+// POST /api/admin/productos
 router.post('/productos', productoController.crearProducto);
 
-//PUT /api/admin/productos
-router.put('/productos/:id', productoController.actualizarProducto);
+// PUT /api/admin/productos /stock
+router.put('/productos', productoController.actualizarProducto);
 
-//patch /api/admin/productos:id/toogle desactivar o activar producto
-router.patch('/productos/:id/toogle', productoController.getProductos);
+// patch /api/admin/productos:id / stock
+router.patch('/productos', productoController.toggleProducto);
 
-//delete /api/admin/productos
-router.delete('/productos/:id', soloAdministrador, productoController.eliminarProducto);
+// delete /api/admin/productos/:id
+router.get('/productos/:id', productoController.eliminarProducto);
 
-//##-----------------------USUARIOS-----------------------##//
 
-//Rutas de Usuarios
-//get /api/admin/usuario
+
+//Rutas de usuario
+// get /api/admin/usuarios
+router.get('/usuarios/estadisticas', usuarioController.getEstadisticasUsuarios);
+
 router.get('/usuarios', usuarioController.getUsuarios);
 
-//get /api/admin/usuarios:id
+// get /api/admin/usuarios
 router.get('/usuarios/:id', usuarioController.getUsuariosById);
 
-//get /api/admin/usuarios/:id/stats
+// get /api/admin/usuarios/:id/stats
 router.get('/usuarios:id/stats', usuarioController.getEstadisticasUsuarios);
 
-//POST /api/admin/usuarios
-router.post('/usuarios', usuarioController.crearUsuario);
+// POST /api/admin/usuarios
+router.post('/usuarios', soloAdministrador, usuarioController.crearUsuario);
 
-//PUT /api/admin/usuarios
-router.put('/usuarios/:id', usuarioController.actualizarUsuario);
+// PUT /api/admin/usuarios
+router.put('/usuarios/:id', soloAdministrador, usuarioController.actualizarUsuario);
 
-//patch /api/admin/usuarios:id/toogle desactivar o activar categoria
-router.patch('/usuarios/:id/toogle', usuarioController.getUsuarios);
+// patch /api/admin/usuario:id / toggle desactivar o activar usuario
+router.patch('/usuarios/:id/toggle', soloAdministrador, usuarioController.toggleUsuario);
 
-//delete /api/admin/Usuarios
+// delete /api/admin/usuario
 router.get('/usuarios/:id', soloAdministrador, usuarioController.eliminarUsuario);
 
-//##-----------------------PEDIDOS (ADMINS)-----------------------##//
 
-//PUT /api/admin/pedidos
-router.put('/pedidos/:id', soloAdministrador, pedidoController.actualizarEstadoPedido);
+//Rutas de pedidos
+// get /api/admin/pedidos/estadistias
+router.get('/pedidos/estadisticas', pedidoController.getEstadisticasPedidos);
 
-//get /api/admin/pedidos
-router.get('/pedidos', soloAdministrador, pedidoController.getAllPedidos);
+// get /api/admin/pedidos
+router.get('/pedidos/:id', pedidoController.getAllPedidos);
 
-//get /api/admin/usuarios/:id/stats
-router.get('/pedidos:id/stats', soloAdministrador, pedidoController.getEstadisticasPedidos);
+// get /api/admin/pedidos/:id
+router.get('/categorias:id/stats', pedidoController.getPedidoById);
 
-//##-----------------------OTROS (pedidos)-----------------------##//
-
-//POST /api/admin/pedidos
-router.post('/pedidos', pedidoController.crearPedido);
-
-//get /api/admin/pedidos
-router.get('/pedidos', pedidoController.getMisPedidos);
-
-//get /api/admin/pedidos:id
-router.get('/pedidos/:id', pedidoController.getPedidoById);
-
-//delete /api/admin/pedidos
-router.get('/pedidos/:id', pedidoController.cancelarPedido);
+// PUT /api/admin/pedidos/:id/estado
+router.put('/pedidos/:id/estado', pedidoController.actualizarEstadoPedido);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = router;
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,9 @@
+/**
+ * Rutas de autenticacion
+ * define los endpoints para registro login y gestion de perfil
+ */
+
+//importar Router de express
+const express = require('express');
+const router = express.Router();
+
